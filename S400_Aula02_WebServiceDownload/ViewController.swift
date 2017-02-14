b888d219c55cb7bbeb6303256f1ade2e22b08433
Swift <swift@valter.local>
2017-02-14 21:04:31 -0200

    Initial Commit

---
+++ S400_Aula02_WebServiceDownload/ViewController.swift
@@ -0,0 +1,25 @@
+//
+//  ViewController.swift
+//  S400_Aula02_WebServiceDownload
+//
+//  Created by Swift on 14/02/17.
+//  Copyright © 2017 Swift. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+
+}
+
